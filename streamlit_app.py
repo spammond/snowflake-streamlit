54b5d88b2155ce0401585991cc2ed1892dff2d0b
spammond <121853745+spammond@users.noreply.github.com>
2023-01-04 13:05:19 +0530

    Fruityvice REST API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("Spammond's Diner")
 
@@ -17,3 +18,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the results on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice's Fruit Advice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
